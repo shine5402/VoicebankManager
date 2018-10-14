db5c1e3c65ca65c8d8af4de5b35c402c626a74c7
Bo <shine_5402@live.cn>
2018-10-14 15:08:46 +0800

    改变版本号

--- public_defines.h
+++ public_defines.h
@@ -2,5 +2,5 @@
 #define PUBLIC_DEFINES_H
 #include <QTextCodec>
 constexpr const char* defaultTextCodecName = "Shift-JIS";
-constexpr const char* voicebankManagerVersion = "0.3.2";
+constexpr const char* voicebankManagerVersion = "0.3.3";
 #endif // PUBLIC_DEFINES_H
